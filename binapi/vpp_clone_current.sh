b13ee0499db3c35c3f776051191234950a4e433f
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2020-05-29 11:14:32 +0200

    Include fixes in calico_plugin
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- binapi/vpp_clone_current.sh
+++ binapi/vpp_clone_current.sh
@@ -4,8 +4,8 @@ if [ ! -d $1 ]; then
 fi
 cd $1
 git reset --hard dcd4aa211
-git fetch "https://gerrit.fd.io/r/vpp" refs/changes/04/27104/3 && git cherry-pick FETCH_HEAD
-git fetch "https://gerrit.fd.io/r/vpp" refs/changes/10/25810/26 && git cherry-pick FETCH_HEAD
-git fetch "https://gerrit.fd.io/r/vpp" refs/changes/85/27085/1 && git cherry-pick FETCH_HEAD
-git fetch "https://gerrit.fd.io/r/vpp" refs/changes/69/27269/1 && git cherry-pick FETCH_HEAD
-git fetch "https://gerrit.fd.io/r/vpp" refs/changes/62/27162/7 && git cherry-pick FETCH_HEAD
+git fetch "https://gerrit.fd.io/r/vpp" refs/changes/04/27104/3 && git cherry-pick FETCH_HEAD # GSO checksum fix
+git fetch "https://gerrit.fd.io/r/vpp" refs/changes/10/25810/26 && git cherry-pick FETCH_HEAD # GRO (coalesce)
+git fetch "https://gerrit.fd.io/r/vpp" refs/changes/85/27085/1 && git cherry-pick FETCH_HEAD # ikev2 cross tunnel fix
+git fetch "https://gerrit.fd.io/r/vpp" refs/changes/69/27269/1 && git cherry-pick FETCH_HEAD # tap interrupt
+git fetch "https://gerrit.fd.io/r/vpp" refs/changes/62/27162/8 && git cherry-pick FETCH_HEAD # calico_plugin
