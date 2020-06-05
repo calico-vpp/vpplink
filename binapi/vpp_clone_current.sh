038540fe6e51a4b25a412bf727486024a59fdab2
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2020-06-05 18:15:10 +0200

    Upgrade to calico_plugin:10
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- binapi/vpp_clone_current.sh
+++ binapi/vpp_clone_current.sh
@@ -11,4 +11,4 @@ git fetch "https://gerrit.fd.io/r/vpp" refs/changes/04/27104/3 && git cherry-pic
 git fetch "https://gerrit.fd.io/r/vpp" refs/changes/10/25810/26 && git cherry-pick FETCH_HEAD # GRO (coalesce)
 git fetch "https://gerrit.fd.io/r/vpp" refs/changes/85/27085/1 && git cherry-pick FETCH_HEAD # ikev2 cross tunnel fix
 git fetch "https://gerrit.fd.io/r/vpp" refs/changes/69/27269/1 && git cherry-pick FETCH_HEAD # tap interrupt
-git fetch "https://gerrit.fd.io/r/vpp" refs/changes/62/27162/9 && git cherry-pick FETCH_HEAD # calico_plugin
+git fetch "https://gerrit.fd.io/r/vpp" refs/changes/62/27162/10 && git cherry-pick FETCH_HEAD # calico_plugin
